0c0aec0d53b2f833e5dcc4cd2f566510b954a6d8
Scott Barron <scott@elitists.net>
2009-01-19 10:37:01 -0500

    combine -vars into -emacs

--- ini/ini-emacs.el
+++ ini/ini-emacs.el
@@ -48,3 +48,9 @@
 (set-default 'indent-tabs-mode nil)
 (set-default 'indicate-empty-lines t)
 (set-default 'imenu-auto-rescan t)
+
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq custom-file (concat dotfiles-dir "custom.el"))
+(setq indent-tabs-mode nil)
+(setq make-backup-files nil)
+(auto-save-mode nil)
--- ini/ini-vars.el
+++
@@ -1,6 +0,0 @@
-;;; Customized Variables
-(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq custom-file (concat dotfiles-dir "custom.el"))
-(setq indent-tabs-mode nil)
-(setq make-backup-files nil)
-(auto-save-mode nil)
