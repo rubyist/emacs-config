9ebfd6c4d94430e0df4ae287cca8c730d2975ed4
Scott Barron <scott@elitists.net>
2009-01-19 11:42:41 -0500

    ruby configs

---
+++ ini/ini-ruby.el
@@ -0,0 +1,13 @@
+(eval-after-load 'ruby-mode
+  '(progn
+     (add-hook 'ruby-mode-hook 'inf-ruby-keys)
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+     (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
+     (define-key ruby-mode-map (kbd "C-c l") "lambda")))
+
+(global-set-key (kbd "C-h r") 'ri)
+
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+
