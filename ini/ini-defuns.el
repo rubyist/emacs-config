09027d7fe9df9e0db68b01d340795ef7b60a01e2
Scott Barron <scott@elitists.net>
2009-01-19 10:48:44 -0500

    rip some more stuff over from ESK

--- ini/ini-defuns.el
+++ ini/ini-defuns.el
@@ -2,3 +2,35 @@
 (defun sab-newline-hook ()
   "Turn this on for a mode to automatically indent while you type."
   (define-key (current-local-map) "\C-m" 'newline-and-indent))
+
+(defun coding-hook ()
+  "Enable things that are convenient across all coding buffers."
+  (setq (make-local-variable 'comment-auto-fill-only-comments) t)
+  (make-local-variable 'column-number-mode)
+  (column-number-mode t)
+  (setq save-place t)
+  (auto-fill-mode)
+  (if window-system (hl-line-mode t)))
+
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+;; TODO belongs in an ido specific config?
+(defun recent-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
--- ini/ini-keys.el
+++ ini/ini-keys.el
@@ -1,1 +1,34 @@
 ;;; Global key bindings
+(global-set-key "\C-\M-h" 'backward-kill-word)
+
+(global-set-key (kbd "C-x \\") 'align-regexp)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+(global-set-key "\C-s" 'isearch-forward-regexp)
+(global-set-key "\C-r" 'isearch-backward-regexp)
+(global-set-key "\C-\M-s" 'isearch-forward)
+(global-set-key "\C-\M-r" 'isearch-backward)
+
+(global-set-key "\C-x\C-i" 'ido-imenu)
+
+(global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
+(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+(global-set-key (kbd "C-x C-p") 'find-file-at-point)
+(global-set-key (kbd "C-c y") 'bury-buffer)
+(global-set-key (kbd "C-c r") 'revert-buffer)
+(global-set-key (kbd "M-`") 'file-cache-minibuffer-complete)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(windmove-default-keybindings) ;; Shift+direction
+(global-set-key "\C-xO" (lambda () (interactive) (other-window -1))) ;; back one
+(global-set-key "\C-x\C-o" (lambda () (interactive) (other-window 2))) ;; forward two
+
+(global-set-key (kbd "C-x m") 'eshell)
+
+(global-set-key (kbd "C-x M") (lambda () (interactive) (eshell t)))
+
+(global-set-key (kbd "C-x M-m") 'shell)
