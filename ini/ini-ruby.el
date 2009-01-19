0eadcaeb721490219e893bcbe02d20f53c3a40b1
Scott Barron <scott@elitists.net>
2009-01-19 16:00:29 -0500

    set up inf-ruby

--- ini/ini-ruby.el
+++ ini/ini-ruby.el
@@ -1,5 +1,10 @@
+(require 'inf-ruby)
+
+(autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
+
 (eval-after-load 'ruby-mode
   '(progn
+     (require 'ruby-compilation)
      (add-hook 'ruby-mode-hook 'inf-ruby-keys)
      (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
      (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
