56365ec8f09f934de119e01ef3b07d186548dd55
Scott Barron <scott@elitists.net>
2009-01-29 11:40:14 -0500

    When opening header files, if the first letter is capitalized use objc-mode otherwise default to c-mode

--- ini/objc.el
+++ ini/objc.el
@@ -3,3 +3,19 @@
                   (sab-newline-hook)
                   (coding-hook)
                   (setq c-basic-offset 4))))
+
+
+;; My conventions for Cocoa are Foo.h - but the auto-mode-alist
+;; searches with sensitivity based on case-fold-search, this function
+;; will check for that and set Foo.h to objc-mode while keeping foo.h
+;; in c-mode.
+(defun determine-header-objc-ness ()
+  (let (sensitivity)
+    (setq sensitivity case-fold-search)
+    (setq case-fold-search nil)
+    (if (string-match "^[A-Z].*\\.h$" (buffer-name))
+        (objc-mode)
+      (c-mode))
+    (setq case-fold-search sensitivity)))
+    
+(add-to-list 'auto-mode-alist '("\\.h$" . determine-header-objc-ness))
